e9d65b5c33ede7a742902d9b6507bb10e8ff4981
sruthikorad36 <sruthikorada36@gmail.com>
2020-03-04 20:49:44 +0000

    insert rows

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,23 +1,23 @@
 INSERT INTO users (name, email, password)
-VALUES (sruthi, srut@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u),
-(vatsalya, vat@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u),
-(martha, martha@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u);
+VALUES ('sruthi', 'srut@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('vatsalya', 'vat@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('martha','martha@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) 
 VALUES (1, 'Blank corner', 'description', 
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 
-  534, 4, 4, 3, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83b80, true),
+  534, 4, 4, 3, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 839780, true),
   (2, 'olives homes', 'description', 
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 
-  852, 6, 6, 7, 'Canada', '51 Main street', 'BC', 'vancouver', 3v5r89, true),
+  852, 6, 6, 7, 'Canada', '51 Main street', 'BC', 'vancouver', 383789, true),
   (1, 'speed lamp', 'description', 
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 
-  134, 1, 2, 1, 'Canada', '651 Yaletown Road', 'Bohbatev', 'Alberta', 83b80, true)
+  134, 1, 2, 1, 'Canada', '651 Yaletown Road', 'Bohbatev', 'Alberta', 837380, true)
 
-  INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
 VALUES (1, 1, '2020-01-2', '2020-01-12'),
 (2, 2, '2020-02-04', '2019-02-10'),
 (3, 3, '2021-10-01', '2021-10-14'); 
